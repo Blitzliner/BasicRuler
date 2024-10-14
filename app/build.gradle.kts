87017d4ad0e978878194e4070cb4b9e645a1e144
Blitzliner <manuel.jasch@gmx.de>
2024-10-14 20:47:25 +0200

    Update target sdk version to 34; Android 14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.maja.basicruler"
         minSdk = 30
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 2
         versionName = "1.1"
 
